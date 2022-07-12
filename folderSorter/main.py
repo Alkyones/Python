b92355589029396ed33f22e18138c83d07c67ac5
Ata Reenes <lifeonshort@gmail.com>
2022-07-12 23:58:16 +0300

    Get rid of sphagetti code and create better structure

--- folderSorter/main.py
+++ folderSorter/main.py
@@ -9,17 +9,44 @@ folderPath = sys.argv[1]
 scheduler = sched.scheduler(time.time, time.sleep)
 
 def FileCounter(folderName):
-    counter = 0
-    for file in os.listdir(folderName):
-        if os.path.isdir(os.path.join(folderName,file)):
-            counter += 1
-            
-    print (counter)
+    date_today = datetime.now().strftime('%Y-%m-%d')
+    hour_today = datetime.now().strftime('%H:%M')
+    _str = f"{date_today}\t{hour_today}\n{folderName}\t"
+
+    ## check files
+    counter_file,counter_dir, etc = 0,0,0
+    for file in os.listdir(folderPath):
+        if os.path.isfile(os.path.join(folderPath,file)):
+            counter_file += 1
+        elif os.path.isdir(os.path.join(folderPath,file)):
+            counter_dir += 1
+        else:
+            etc += 1
     
-    scheduler.enter(30, 1, FileCounter, kwargs={'folderName': folderPath})
+    #format str
+    if counter_file > 0:
+        _str += f"has {counter_file} file(s)\t"
+    
+    if counter_dir > 0:
+        _str += f"has {counter_dir} folder(s)\t"
+
+    if etc > 0:
+        _str += f"has {etc} unknown type file(s)\t"
 
 
-scheduler.enter(30, 1, FileCounter, kwargs={'folderName': folderPath}) 
+    print(_str)
+
+
+    
+
+
+
+
+    print (_str)
+    scheduler.enter(3600, 1, FileCounter, kwargs={'folderName': folderPath})
+
+
+scheduler.enter(1, 1, FileCounter, kwargs={'folderName': folderPath}) 
 scheduler.run()
 
 
