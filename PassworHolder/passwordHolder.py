212545e5362571f022ccdc53d0b66bd99c2d959d
Atakan <atakan.yildirim@frupro.com>
2023-09-24 12:18:54 +0300

    Removed not used scripts and fixed typos

---
+++ PassworHolder/passwordHolder.py
@@ -0,0 +1,41 @@
+from pymongo import MongoClient
+
+
+
+
+#connections
+def discounnectDb(db):
+    try:
+        db.close()
+        return True
+    except:
+        return False
+
+def connectDb() :
+    dbUrl = 'mongodb+srv://lifeonshort:29CpgC2Qr563KIwQ@pythonprojects.ws0xsk7.mongodb.net/'
+    databaseName = "PasswordHolder"
+    db = MongoClient(dbUrl)
+    database = db.PasswordHolder
+    print("Connected to Mongo")
+    return database
+
+def connectCollection(dbQ):
+    return dbQ.passwords
+#insert  / remove
+
+def insertPassword(): pass
+
+def updatePassword(): pass
+
+def removePassword(): pass
+
+def listProgram(userDesicion="X"):
+    print("Please select the action you want to perform...\n")
+    print("1- Show saved passwords\n2- Search saved password\n3- Insert new password\n4- Update saved password\n5- Delete saved password")
+
+
+db =  connectDb()
+collection = connectCollection(db)
+
+
+discounnectDb(db) 
