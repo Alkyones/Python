51e89bfc76d9208ae6601407e2042810952ebc59
Atakan <atakan.yildirim@frupro.com>
2023-10-17 22:21:05 +0300

    removed unused scripts

--- Challenges/isUnique.py
+++
@@ -1,8 +0,0 @@
-def has_unique_characters(keyword):
-    list_keyword = list(keyword)
-    set_keyword = set(list_keyword)
-    
-    return (len(list_keyword) == len(set_keyword))
-    
-print(has_unique_characters("abcdefg"))  # Output: True
-print(has_unique_characters("hello"))    # Output: False
--- Challenges/reverse_sentence.py
+++
@@ -1,8 +0,0 @@
-def reverse_sentence(string):
-    listStr = string.split(' ')[::-1]
-    res = ' '.join(listStr)
-    return res
-
-
-print(reverse_sentence("Hello world!"))  # Output: "world! Hello"
-print(reverse_sentence("Python is awesome"))  # Output: "awesome is Python"
