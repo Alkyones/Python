7e9f8526ba09044f8421c12d7377d4157d015395
Atakan <atakan.yildirim@frupro.com>
2023-11-05 19:09:42 +0300

    Pipeline added

--- WebScrabing/AmazonProductCPipeLine/index.py
+++ WebScrabing/AmazonProductCPipeLine/index.py
@@ -1,10 +1,22 @@
 import os, sys
 import json
 from database import Database
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
 credentialFile = os.path.abspath(os.path.join(
                                               os.path.dirname(os.path.realpath(__file__)),
                                               '../../credentials')
                                  )
+def getLinksFromList(data):
+    links = []
+    for el in data:
+        link = el.find('a')
+        if link: links.append(link['href'])
+    print(links)
+    
 def get_credentials():
     try:
         with open(f'{credentialFile}/amazonPipeline.json', 'r') as file:
@@ -17,13 +29,25 @@ def get_credentials():
 
 
 credentials= get_credentials()
-if credentials is None:
-    print('No credentials found hence program is closed.')
-    sys.exit("No credentials found hence program is closed.")
+if credentials is None: sys.exit("No credentials found hence program is closed.")
 
+#DB connection
 DB = Database(credentials)
 
+url= "https://www.amazon.com.tr/gp/bestsellers?ref_=nav_cs_bestsellers"
 
+
+
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')
+driver = webdriver.Chrome()
+driver.get(url)
+
+page_source = driver.page_source
+soup = BeautifulSoup(page_source, 'lxml')
+
+containerData = soup.find_all('div', {'class': 'a-size-base a-inline-block'})
+getLinksFromList(containerData)
 
 
 
--- WebScrabing/ifeSc/index.py
+++ WebScrabing/ifeSc/index.py
@@ -1,5 +1,4 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 import time
 import pandas as pd
